2a00f9c1157505e0bd62756608fe3a8c6bf55283
mmklzmant <mmklzmant@163.com>
2017-08-09 18:16:35 +0800

    模仿小米菜单二级导航initial

---
+++ 模仿小米二级菜单导航/data/goods-nav.js
@@ -0,0 +1,23 @@
+/**
+ * 功能：商品数据
+ * @authors mmklzmant (mmklzmant@163.com)
+ * @date    2017-08-09 18:07:54
+ */
+
+var goodsNav = [
+	{
+		classfiy: "手机 电话卡",
+		goods: [
+			{
+				name: "小米六"
+			},
+			{
+				name: "小米5X"
+			}
+
+		]
+	},
+	{
+		
+	}
+];
