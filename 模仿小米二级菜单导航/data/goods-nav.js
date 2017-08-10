3b440346ddb2cea1785e1bed5db2cd09a9dc60e6
mmklzmant <mmklzmant@163.com>
2017-08-10 18:16:14 +0800

    加载子菜单

--- 模仿小米二级菜单导航/data/goods-nav.js
+++ 模仿小米二级菜单导航/data/goods-nav.js
@@ -5,26 +5,32 @@
  */
 //商品数据
 var goodsNav = [
-	{
+    {
         classfiy: "手机 电话卡",
         goods: [
-        	{
-                name: "小米六"
+            {
+                name: "小米六",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/xm6_80.png?width=40&height=40"
             },
             {
-                name: "小米5X"
+                name: "小米5X",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/80pc-5x.png?width=40&height=40"
             },
             {
-                name: "小米Max 2"
+                name: "小米Max 2",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/max2_80.jpg?width=40&height=40"
             },
             {
-                name: "小米手机5c"
+                name: "小米手机5c",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/5c_80.png?width=40&height=40"
             },
             {
-                name: "小米手机5c"
+                name: "小米手机5c",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/xmNOTE2-80.jpg?width=40&height=40"
             },
             {
-                name: "小米Note2"
+                name: "小米Note2",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/MIX-80.jpg?width=40&height=40"
             }
 
         ]
@@ -32,23 +38,29 @@ var goodsNav = [
     {
         classfiy: "笔记本 平板",
         goods: [
-        	{
-                name: "笔记本12.5英寸"
+            {
+                name: "笔记本12.5英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/bijiben80.jpg?width=40&height=40"
             },
             {
-                name: "笔记本13.3英寸"
+                name: "笔记本13.3英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/bijiben80.jpg?width=40&height=40" 
             },
             {
-                name: "小米平板3"
+                name: "小米平板3",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/mipad3_80.jpg?width=40&height=40"
             },
             {
-                name: "USB-C转接器"
+                name: "USB-C转接器",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/usbzjqggg80.jpg?width=40&height=40"
             },
             {
-                name: "鼠标垫"
+                name: "鼠标垫",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/xmsbd80.jpg?width=40&height=40"
             },
             {
-                name: "小米无线鼠标"
+                name: "小米无线鼠标",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/wxsb80.png?width=40&height=40"
             }
 
         ]
@@ -56,23 +68,29 @@ var goodsNav = [
     {
         classfiy: "电视 盒子",
         goods: [
-        	{
-                name: "小米电视4 49英寸"
+            {
+                name: "小米电视4 49英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/80xmds_49.jpg?width=40&height=40"
             },
             {
-                name: "小米电视4 55英寸"
+                name: "小米电视4 55英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/80xmds_55.jpg?width=40&height=40"
             },
             {
-                name: "小米电视4 65英寸"
+                name: "小米电视4 65英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/80xmds_65.jpg?width=40&height=40"
             },
             {
-                name: "小米电视4A 32英寸"
+                name: "小米电视4A 32英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/zuodaohang.jpg?width=40&height=40"
             },
             {
-                name: "小米电视4A 55英寸"
+                name: "小米电视4A 55英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/xp80_49.jpg?width=40&height=40"
             },
             {
-                name: "小米电视4A 49英寸"
+                name: "小米电视4A 49英寸",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/xp80_55.jpg?width=40&height=40"
             }
 
         ]
@@ -80,23 +98,29 @@ var goodsNav = [
     {
         classfiy: "路由器 智能硬件",
         goods: [
-        	{
-                name: "九号平衡车"
+            {
+                name: "九号平衡车",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/phcplus80.jpg?width=40&height=40"
             },
             {
-                name: "米家扫地机器人及配件"
+                name: "米家扫地机器人及配件",
+                imgUrl: "https://c1.mifile.cn/f/i/g/doodle/jiqiren.jpg?width=40&height=40"
             },
             {
-                name: "小米VR眼镜"
+                name: "小米VR眼镜",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/VRPLAY2.png?width=40&height=40"
             },
             {
-                name: "小米路由器"
+                name: "小米路由器",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/luyouqi-80.jpg?width=40&height=40"
             },
             {
-                name: "小米音响"
+                name: "手环及配件",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/video/shouhuan280.jpg?width=40&height=40"
             },
             {
-                name: "手表"
+                name: "手表",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/dianhuashoubiao80.jpg?width=40&height=40"
             }
 
         ]
@@ -104,46 +128,58 @@ var goodsNav = [
     {
         classfiy: "移动电源 电线 插线板",
         goods: [
-        	{
-                name: "小米移动电源"
+            {
+                name: "小米移动电源",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/dianyuan280.jpg?width=40&height=40"
             },
             {
-                name: "插线板"
+                name: "插线板",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/c1xb343.jpg?width=40&height=40"
             },
             {
-                name: "品牌移动电源"
+                name: "品牌移动电源",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/yidongdianyuan.jpg?width=40&height=40"
             },
             {
-                name: "移动电源附件"
+                name: "移动电源附件",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/dianyuanfujian.jpg?width=40&height=40"
             },
             {
-                name: "充电线"
+                name: "充电线",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/chongdianqi-80.jpg?width=40&height=40"
             },
             {
-                name: "彩虹五号电池"
+                name: "彩虹五号电池",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/5Battery1.jpg?width=40&height=40"
             }
         ]
     },
     {
         classfiy: "耳机 音响",
         goods: [
-        	{
-                name: "小米头戴式耳机"
+            {
+                name: "小米头戴式耳机",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/toudai80.jpg?width=40&height=40"
             },
             {
-                name: "头戴式耳机轻松版"
+                name: "头戴式耳机轻松版",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/tdsqs80.jpg?width=40&height=40"
             },
             {
-                name: "小米降噪耳机"
+                name: "小米降噪耳机",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/xmjzej80.jpg?width=40&height=40"
             },
             {
-                name: "小米圈铁耳机Pro"
+                name: "小米圈铁耳机Pro",
+                imgUrl: "https://c1.mifile.cn/f/i/g/2015/cn-index/qtpro80.jpg?width=40&height=40"
             },
             {
-                name: "小米胶囊耳机"
+                name: "小米胶囊耳机",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/jiaonang80.jpg?width=40&height=40"
             },
             {
-                name: "小米蓝牙耳机"
+                name: "小米蓝牙耳机",
+                imgUrl: "https://c1.mifile.cn/f/i/15/goods/sidebar/bluetoothheadset.jpg?width=40&height=40"
             }
         ]
     }
--- 模仿小米二级菜单导航/javascripts/index.js
+++ 模仿小米二级菜单导航/javascripts/index.js
@@ -1,5 +1,5 @@
 /**
- * 功能：
+ * 功能：二级菜单导航
  * @authors mmklzmant (mmklzmant@163.com)
  * @date    2017-08-09 10:01:13
  */
@@ -10,14 +10,26 @@
 /*************************************************/
 //主菜单menu
 var menu = document.getElementById("menu");
+var submenu = document.getElementById("submenu");
 
 
 /*************************************************/
 /* 函数调用部分 */
 /*************************************************/
 //加载主菜单
+// console.log(goodsNav);
 createMenu();
 
+// 主菜单hover事件 冒泡
+/*menu.onmouseover = function(e) {
+    var e = e || window.event;
+    var target = e.target || e.srcElement;
+    if (target.nodeName.toLowerCase() == "li") {
+        var str = target.textContent;
+        //加载子菜单
+        createSubmenu(str.slice(0, str.indexOf("&")));
+    }
+}*/
 /*************************************************/
 /* 功能函数及方法定义部分 */
 /*************************************************/
@@ -25,25 +37,55 @@ createMenu();
 /**
  * 功能： 加载主菜单
  */
-function createMenu(){
-	//主菜单长度
-	var len = goodsNav.length;
-	//html
-	var html = "";
-	for(var i = 0; i < len; i++)
-	{
-		html += "<li><a href=\"#\">" + 
-		     goodsNav[i].classfiy + "</a>";
-		//goods
-		var goods = goodsNav[i].goods;
-		var goods_len = goods.length;
-		html += "<ul id=\"submenu\">"
-		/*for(var j = 0; j < goods_len; j++)
-		{
-			html += "<li><a href=\"#\">" + goods[j].name + "</a></li>"
-		}*/
-		html += "</ul>";
-	}
-	
-	menu.innerHTML = html;
+function createMenu() {
+    //主菜单长度
+    var len = goodsNav.length;
+    //html
+    var html = "";
+    for (var i = 0; i < len; i++) {
+        html += "<li><a href=\"#\">" +
+            goodsNav[i].classfiy + "<i class=\"iconfont\">&#xe6a7;</i></a>";
+    }
+    menu.innerHTML = html;
+    //绑定鼠标事件
+    for (var j = 0; j < len; j++) {
+        var menu_li = menu.getElementsByTagName("li")[j];
+        menu_li.index = j;
+        menu_li.onmouseenter = function() {
+
+            createSubmenu(this.index);
+        }
+    }
+
 }
+
+//生成子菜单
+function createSubmenu(index) {
+    var len = goodsNav.length;
+    var goodsItem = goodsNav[index].goods;
+    var html = "";
+    var goods_len = goodsItem.length;
+    for (var j = 0; j < goods_len; j++) {
+        html += "<li><img src=\"" + goodsItem[j].imgUrl +
+            "\"><a href=\"#\">" + goodsItem[j].name + "</a></li>";
+    }
+    submenu.innerHTML = html;
+}
+
+//事件冒泡
+/*function createSubmenu(text) {
+    var len = goodsNav.length;
+    for (var i = 0; i < len; i++) {
+        if (text === goodsNav[i].classfiy) {
+        	var goodsItem = goodsNav[i].goods;
+        	var html = "";
+            var goods_len = goodsItem.length;
+            for (var j = 0; j < goods_len; j++) {
+				html += "<li><img src=\"" + goodsItem[j].imgUrl +
+				"\"><a href=\"#\">" + goodsItem[j].name + "</a></li>";
+            }
+            console.log(html);
+            submenu.innerHTML = html;
+        }
+    }
+}*/
