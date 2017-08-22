ebd4e3e62750a247a4ec9b7a75d7a63e7400bd30
mmklzmant <mmklzmant@163.com>
2017-08-22 18:39:29 +0800

    update push

--- sort/Sort.java
+++ sort/Sort.java
@@ -8,7 +8,15 @@ public class Sort{
 			return;
 		}
 		// 逆序
-		for(int i = 0; i < arr.length-1; i++)
+		for(int i = 0; i < arr.length-1; i++){
+			for(int j = arr.length-1; j > i; j--){
+				if(arr[j] < arr[j-1]){
+					swap(arr, j, j-1);
+				}
+			}
+		}
+
+		/*for(int i = 0; i < arr.length-1; i++)
 		{
 			for(int j = arr.length-1; j > i; j--){
 				if(arr[j] < arr[j-1])
@@ -16,7 +24,7 @@ public class Sort{
 					swap(arr, j-1, j);
 				}
 			}
-		}
+		}*/
 		// 顺序
 		/*for(int i = arr.length-1; i > 0; i--){
 			for(int j = 0; j < i; j++){
@@ -28,12 +36,74 @@ public class Sort{
 		print(arr);
 	}
 
-	public static void swap(int[] arr, int i, int j){
-		int temp = arr[j];
-		arr[j] = arr[i];
-		arr[i] = temp;
+	/**
+	 * 功能：选择排序
+	 * @param arr [description]
+	 */
+	public static void selectSort(int[] arr){
+		if(arr == null || arr.length == 0){
+			return;
+		}
+		int minIndex = 0;
+		for(int i = 0; i < arr.length-1; i++){
+			minIndex = i;
+			for(int j = i+1; j < arr.length; j++){
+				if(arr[j] < arr[minIndex])
+				{
+					minIndex = j;
+				}
+			}
+
+			if(minIndex != i)
+			{
+				swap(arr, i, minIndex);
+			}
+		}
+		print(arr);
 	}
 
+	/**
+	 * 功能：插入排序
+	 * @param arr [description]
+	 */
+	public static void insertSort(int[] arr){
+		if(arr == null || arr.length == 0){
+			return;
+		}
+
+		for(int i = 0; i < arr.length; i++)
+		{
+			int j = i;
+			int target = arr[i];
+
+			while(j > 0 && target < arr[j-1]){
+				arr[j] = arr[j-1];
+				j--;
+			}
+			arr[j] = target;
+		}
+		print(arr);
+	}
+	/**
+	 * 功能：交换位置
+	 * @param arr [数组]
+	 * @param i   [下标]
+	 * @param j   [下标]
+	 */
+	public static void swap(int[] arr, int i, int j){
+		arr[i] = arr[i]^arr[j];
+		arr[j] = arr[i]^arr[j];
+		arr[i] = arr[i]^arr[j];
+
+		/*int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;*/
+	}
+
+	/**
+	 * 打印
+	 * @param arr [description]
+	 */
 	public static void print(int[] arr){
 		for(int i = 0; i < arr.length; i++){
 			System.out.print(arr[i] + " ");
--- sort/Test.java
+++ sort/Test.java
@@ -1,6 +1,11 @@
 public class Test{
 	public static void main(String[] args) {
 		int arr[] = {2, 5, 6, 1, 8, 7, 6};
-		Sort.bubbleSort(arr);
+		//冒泡排序
+		// Sort.bubbleSort(arr);
+		//选择排序
+		// Sort.selectSort(arr);
+		// 插入排序
+		Sort.insertSort(arr);
 	}
 }
